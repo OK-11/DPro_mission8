60be032bfc2e99ad2638ceeba9d2dfdcc3edabf6
尾澤 公佑 <koushuke06@gmail.com>
2024-08-27 16:16:47 +0900

    initコミット

---
+++ javascripts/ajax_sample.js
@@ -0,0 +1,43 @@
+let number = 0;
+const videoArea = document.getElementById("video");
+const titleArea = document.getElementById("title");
+const contentArea = document.getElementById("content");
+const button = document.getElementById('btn');
+
+let jsonDate;
+
+function getData() {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      if(request.status == 200) {
+        jsonDate = request.response;
+      }
+    }
+  }
+  request.open("GET", "ajax.json");
+  request.responseType = "json";
+  request.send(null);
+}
+
+
+function changeVideo() {
+  // ボタンがクリックされた際の処理を記述
+  // ajax.jsonからデータを取得していない場合のみ、getDataの処理を呼び出す
+    button.addEventListener('click', e => {
+        if (jsonDate==null) {
+          getData();
+        } else {
+          titleArea.innerHTML = jsonDate[number].title;
+          contentArea.innerHTML = jsonDate[number].content;
+          videoArea.setAttribute("src", jsonDate[number].url);
+          if (number==2) {
+            number = 0;
+          } else {
+            number++;
+          }
+        }   
+    })
+}
+
+window.onload = changeVideo;
